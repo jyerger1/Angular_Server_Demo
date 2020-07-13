cb52cd9aa6f1962e0808c720b2e1b910453c0bdc
John Yerger <johnyerger@johns-mbp.home>
2020-07-13 14:30:36 -0400

    directives

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,15 +1,31 @@
 import { Component } from '@angular/core';
+import { preserveWhitespacesDefault } from '@angular/compiler';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
+  styles: [
+    `
+      .online {
+        color: white;
+      }
+    `,
+  ],
 })
 export class ServerComponent {
   // data binding
   serverId = 10;
   serverStatus = 'offline';
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
